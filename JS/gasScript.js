de95f0ddc6b34b02bf10b10da60282c46f8ca1c9
Jory <user@ulysses.local>
2017-07-11 20:42:58 -0400

    stagnation values for full mode

--- JS/gasScript.js
+++ JS/gasScript.js
@@ -1,7 +1,13 @@
-// Global vars
+//--------------------------------------------------------
+// GLOBAL VARIABLES
+//--------------------------------------------------------
 var fullMode = false;
 var showStag = false;
 var showShock = false;
+
+//--------------------------------------------------------
+// READING
+//--------------------------------------------------------
 
 // Obtain and return inputs
 function getInputs() {
@@ -17,7 +23,21 @@ function getInputs() {
 	return [gammaVal, machVal];
 }
 
+// Obtain and return optional inputs
+function getOptInputs() {
+	console.log("getting optional inputs");
+	var tempField  = document.getElementById('temp');
+	var densField  = document.getElementById('dens');
+	var pressField = document.getElementById('press');
+	var tempVal = tempField.value;
+	var densVal = densField.value;
+	var pressVal = pressField.value;
+	return [tempVal, densVal, pressVal];
+}
+
+//----------------------------------------------------------
 // Isentropic relations
+//----------------------------------------------------------
 function calcTotalTemperature(gam,mach,tem) {
 	var tratio = (1.0 + ((gam-1.0)/2.0)*mach**2)
 	return tratio * tem;
@@ -31,7 +51,9 @@ function calcTotalPressure(gam,mach,press) {
 	return pratio * press;
 }
 
-// Normal shock wave relations
+//-----------------------------------------------------------
+// Normal shock relations
+//-----------------------------------------------------------
 function calcShockTemperature(gam,mach,tem) {
 	var tratio = (1.0+((gam-1.0)/2.0)*mach**2)*((2.0*gam/(gam-1.0))*mach**2-1.0)/
 		((mach**2)*((2.0*gam/(gam-1.0))+(gam-1.0)/2.0));
@@ -60,8 +82,11 @@ function displayToggle(trigger,elem,btn) {
 	return trigger;
 }
 
+//----------------------------------------------------------
+// Compressible gasdynamic Equation wrappers
+//----------------------------------------------------------
 // Wrapper that gets all of the total properties given gamma and mach
-function calcTotalProperties(gam,mach) {
+function calcTotalRatios(gam,mach) {
 	var ttemp = 0;
 	var tdens = 0;
 	var tpress = 0;
@@ -72,7 +97,7 @@ function calcTotalProperties(gam,mach) {
 }
 
 // Wrapper that gets all of the shock properties given gamma and mach
-function calcShockProperties(gam,mach) {
+function calcShockRatios(gam,mach) {
 	var stemp = 0;
 	var sdens = 0;
 	var spress = 0;
@@ -82,7 +107,20 @@ function calcShockProperties(gam,mach) {
 	return [stemp,sdens,spress];
 }
 
-// Main program
+// Wrapper that gets the total values given gamma, mach, temp, dens and press
+function calcTotalValues(gam,mach,temp,dens,press) {
+	var temp0 = 0;
+	var dens0 = 0;
+	var press0 = 0;
+	temp0 = calcTotalTemperature(gam,mach,temp);
+	dens0 = calcTotalDensity(gam,mach,dens);
+	press0 = calcTotalPressure(gam,mach,press);
+	return [temp0,dens0,press0];
+}
+
+//--------------------------------------------------------------
+// MAIN PROGRAM
+//--------------------------------------------------------------
 document.addEventListener("DOMContentLoaded", function(){
 	// button elements
 	var computeBtn  = document.getElementById('compute');
@@ -94,10 +132,16 @@ document.addEventListener("DOMContentLoaded", function(){
 	var fullResult   = document.getElementById('more-total-properties');
 	var stagResults  = document.getElementById('stag-results');
 	var shockResults = document.getElementById('shock-results');
-	// fields
+	// FIELDS
+	// Stagnation ratios
 	var ttempField   = document.getElementById('ttemp');
 	var tdensField   = document.getElementById('tdens');
 	var tpressField  = document.getElementById('tpress');
+	// Stagnation values
+	var ttempValField = document.getElementById('T0');
+	var tdensValField = document.getElementById('d0');
+	var tpressValField = document.getElementById('P0');
+	// Shock Ratios
 	var stempField   = document.getElementById('stemp');
 	var sdensField   = document.getElementById('sdens');
 	var spressField  = document.getElementById('spress');
@@ -105,25 +149,35 @@ document.addEventListener("DOMContentLoaded", function(){
 	var ttemp;
 	var tdens;
 	var tpress;
+	var temp0;
+	var dens0;
+	var press0;
 	var stemp;
 	var sdens;
 	var spress;
 	// Calculate
 	computeBtn.onclick = function() {
 		[gamma,mach] = getInputs();
-		[ttemp,tdens,tpress] = calcTotalProperties(gamma,mach);
-		[stemp,sdens,spress] = calcShockProperties(gamma,mach);
-		ttempField.value = ttemp.toFixed(2);
-		tdensField.value = tdens.toFixed(2);
+		[ttemp,tdens,tpress] = calcTotalRatios(gamma,mach);
+		[stemp,sdens,spress] = calcShockRatios(gamma,mach);
+		ttempField.value  = ttemp.toFixed(2);
+		tdensField.value  = tdens.toFixed(2);
 		tpressField.value = tpress.toFixed(2);
-		stempField.value = stemp.toFixed(2);
-		sdensField.value = sdens.toFixed(2);
+		stempField.value  = stemp.toFixed(2);
+		sdensField.value  = sdens.toFixed(2);
 		spressField.value = spress.toFixed(2);
+		// Compute stagnation values if full mode is on
+		if (fullMode === true) {
+			[temp0,dens0,press0] = getOptInputs();
+			[temp0,dens0,press0] = calcTotalValues(gamma,mach,temp0,dens0,press0);
+			ttempValField.value  = temp0.toFixed(2);
+			tdensValField.value  = dens0.toFixed(2);
+			tpressValField.value = press0.toFixed(2);
+		};
 	};
 
 	// Toggle full display
 	modeBtn.onclick = function() {
-		/*
 		fullMode = ! fullMode;
 		if ( fullMode ) {
 			fullDiv.style.display = "block";
@@ -133,8 +187,7 @@ document.addEventListener("DOMContentLoaded", function(){
 			fullDiv.style.display = "none";
 			fullResult.style.display = "none";
 			modeBtn.value = "full";
-		}
-		*/
+		};
 	};
 
 	// Show/hide total properties
